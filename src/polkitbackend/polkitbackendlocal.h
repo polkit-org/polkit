f1026325a38b47625d8d0375dd6739a1e41e2df9
David Zeuthen <davidz@redhat.com>
2008-12-03 14:01:12 -0500

    forgot to add local backend impl

---
+++ src/polkitbackend/polkitbackendlocal.h
@@ -0,0 +1,75 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
+
+/*
+ * Copyright (C) 2008 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: David Zeuthen <davidz@redhat.com>
+ */
+
+#if !defined (_POLKIT_BACKEND_COMPILATION) && !defined(_POLKIT_BACKEND_INSIDE_POLKIT_BACKEND_H)
+#error "Only <polkitbackend/polkitbackend.h> can be included directly, this file may disappear or change contents."
+#endif
+
+#include <glib-object.h>
+
+#ifndef __POLKIT_BACKEND_LOCAL_H
+#define __POLKIT_BACKEND_LOCAL_H
+
+G_BEGIN_DECLS
+
+#define POLKIT_TYPE_BACKEND_LOCAL         (polkit_backend_local_get_type ())
+#define POLKIT_BACKEND_LOCAL(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), POLKIT_TYPE_BACKEND_LOCAL, PolkitBackendLocal))
+#define POLKIT_BACKEND_LOCAL_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST ((k), POLKIT_TYPE_BACKEND_LOCAL, PolkitBackendLocalClass))
+#define POLKIT_BACKEND_LOCAL_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), POLKIT_TYPE_BACKEND_LOCAL,PolkitBackendLocalClass))
+#define POLKIT_IS_BACKEND_LOCAL(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), POLKIT_TYPE_BACKEND_LOCAL))
+#define POLKIT_IS_BACKEND_LOCAL_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), POLKIT_TYPE_BACKEND_LOCAL))
+
+typedef struct _PolkitBackendLocal         PolkitBackendLocal;
+typedef struct _PolkitBackendLocalClass    PolkitBackendLocalClass;
+
+struct _PolkitBackendLocal
+{
+        GObject parent_instance;
+};
+
+struct _PolkitBackendLocalClass
+{
+        GObjectClass parent_class;
+
+        /*< public >*/
+
+        /*< private >*/
+        /* Padding for future expansion */
+        void (*_polkit_reserved1) (void);
+        void (*_polkit_reserved2) (void);
+        void (*_polkit_reserved3) (void);
+        void (*_polkit_reserved4) (void);
+        void (*_polkit_reserved5) (void);
+        void (*_polkit_reserved6) (void);
+        void (*_polkit_reserved7) (void);
+        void (*_polkit_reserved8) (void);
+};
+
+GType               polkit_backend_local_get_type (void) G_GNUC_CONST;
+
+PolkitBackendLocal *polkit_backend_local_new      (void);
+
+G_END_DECLS
+
+#endif /* __POLKIT_BACKEND_LOCAL_H */
+
