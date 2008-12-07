d5a8a9bd2e9ee995995b911af68d0b7614ad64cb
David Zeuthen <davidz@redhat.com>
2008-12-07 13:14:08 -0500

    turn PolkitActionDescription into an user defined struct
    Now to port the XML parser and land it in polkitbackend so backends
    can easily implement EnumerateActions().

--- src/polkit/polkitactiondescription.h
+++ src/polkit/polkitactiondescription.h
@@ -19,55 +19,40 @@
  * Author: David Zeuthen <davidz@redhat.com>
  */
 
-#if !defined (_POLKIT_COMPILATION) && !defined(_POLKIT_INSIDE_POLKIT_H)
-#error "Only <polkit/polkit.h> can be included directly, this file may disappear or change contents."
-#endif
+#ifndef __POLKIT_ACTION_DESCRIPTION_H
+#define __POLKIT_ACTION_DESCRIPTION_H
 
-#ifndef __POLKIT_ACTION_DESCRIPTION_H__
-#define __POLKIT_ACTION_DESCRIPTION_H__
-
+#include <sys/types.h>
+#include <unistd.h>
 #include <glib-object.h>
-#include <gio/gio.h>
+#include <polkit/polkitbindings.h>
 
 G_BEGIN_DECLS
 
-#define POLKIT_TYPE_ACTION_DESCRIPTION         (polkit_action_description_get_type ())
+#define POLKIT_TYPE_ACTION_DESCRIPTION         (polkit_action_description_get_type())
 #define POLKIT_ACTION_DESCRIPTION(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), POLKIT_TYPE_ACTION_DESCRIPTION, PolkitActionDescription))
-#define POLKIT_ACTION_DESCRIPTION_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), POLKIT_TYPE_ACTION_DESCRIPTION, PolkitActionDescriptionClass))
 #define POLKIT_IS_ACTION_DESCRIPTION(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), POLKIT_TYPE_ACTION_DESCRIPTION))
-#define POLKIT_IS_ACTION_DESCRIPTION_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), POLKIT_TYPE_ACTION_DESCRIPTION))
-#define POLKIT_ACTION_DESCRIPTION_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), POLKIT_TYPE_ACTION_DESCRIPTION, PolkitActionDescriptionClass))
+#define POLKIT_ACTION_DESCRIPTION_GET_IFACE(o) (G_TYPE_INSTANCE_GET_INTERFACE((o), POLKIT_TYPE_ACTION_DESCRIPTION, PolkitActionDescriptionIface))
 
-typedef struct _PolkitActionDescription        PolkitActionDescription;
-typedef struct _PolkitActionDescriptionClass   PolkitActionDescriptionClass;
-typedef struct _PolkitActionDescriptionPrivate PolkitActionDescriptionPrivate;
+#if 0
+typedef struct _PolkitActionDescription PolkitActionDescription; /* Dummy typedef */
+#endif
+typedef struct _PolkitActionDescriptionIface PolkitActionDescriptionIface;
 
-struct _PolkitActionDescription
+struct _PolkitActionDescriptionIface
 {
-  GObject                         parent_instance;
-  PolkitActionDescriptionPrivate *priv;
+  GTypeInterface g_iface;
 };
 
-struct _PolkitActionDescriptionClass
-{
-  GObjectClass parent_class;
-};
-
-GType                      polkit_action_description_get_type           (void) G_GNUC_CONST;
-GList *                    polkit_action_description_new_from_file      (GFile                    *file,
-                                                                         GCancellable             *cancellable,
-                                                                         GError                  **error);
-GList *                    polkit_action_description_new_from_directory (GFile                    *directory,
-                                                                         GCancellable             *cancellable,
-                                                                         GError                  **error);
-const gchar *              polkit_action_description_get_action_id      (PolkitActionDescription  *action_description);
-GIcon *                    polkit_action_description_get_icon           (PolkitActionDescription  *action_description);
-const gchar *              polkit_action_description_get_description    (PolkitActionDescription  *action_description);
-const gchar *              polkit_action_description_get_message        (PolkitActionDescription  *action_description);
-const gchar *              polkit_action_description_get_vendor_name    (PolkitActionDescription  *action_description);
-const gchar *              polkit_action_description_get_vendor_url     (PolkitActionDescription  *action_description);
-GHashTable *               polkit_action_description_get_annotations    (PolkitActionDescription  *action_description);
+GType         polkit_action_description_get_type         (void) G_GNUC_CONST;
+const gchar  *polkit_action_description_get_action_id    (PolkitActionDescription *action_description);
+const gchar  *polkit_action_description_get_description  (PolkitActionDescription *action_description);
+const gchar  *polkit_action_description_get_message      (PolkitActionDescription *action_description);
+const gchar  *polkit_action_description_get_vendor_name  (PolkitActionDescription *action_description);
+const gchar  *polkit_action_description_get_vendor_url   (PolkitActionDescription *action_description);
+GIcon        *polkit_action_description_get_icon         (PolkitActionDescription *action_description);
+GHashTable   *polkit_action_description_get_annotations  (PolkitActionDescription *action_description);
 
 G_END_DECLS
 
-#endif /* __POLKIT_ACTION_DESCRIPTION_H__ */
+#endif /* __POLKIT_ACTION_DESCRIPTION_H */
