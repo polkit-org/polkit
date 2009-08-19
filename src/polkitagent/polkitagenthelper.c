c7ded649ab294d8960ebf3be98461f509c10bdf1
David Zeuthen <davidz@redhat.com>
2009-08-19 13:47:01 -0400

    Use correct program name when complaining about not being setuid root

--- src/polkitagent/polkitagenthelper.c
+++ src/polkitagent/polkitagenthelper.c
@@ -81,7 +81,7 @@ main (int argc, char *argv[])
   /* check that we are setuid root */
   if (geteuid () != 0)
     {
-      fprintf (stderr, "polkit-grant-helper-pam: needs to be setuid root\n");
+      fprintf (stderr, "polkit-agent-helper-1: needs to be setuid root\n");
       goto error;
     }
 
