a2bf5c9c83b6ae46cbd5c779d3055bff81ded683
Jan Rybar <jrybar@redhat.com>
2022-01-25 17:21:46 +0000

    pkexec: local privilege escalation (CVE-2021-4034)

--- src/programs/pkcheck.c
+++ src/programs/pkcheck.c
@@ -363,6 +363,11 @@ main (int argc, char *argv[])
   local_agent_handle = NULL;
   ret = 126;
 
+  if (argc < 1)
+    {
+      exit(126);
+    }
+
   /* Disable remote file access from GIO. */
   setenv ("GIO_USE_VFS", "local", 1);
 
