a6bedfd09b7bba753de7a107dc471da0db801858
Xi Ruoyao <xry111@mengyan1223.wang>
2022-01-27 10:16:32 +0000

    jsauthority: port to mozjs-91

--- src/polkitbackend/polkitbackendjsauthority.cpp
+++ src/polkitbackend/polkitbackendjsauthority.cpp
@@ -53,6 +53,13 @@
 
 /* ---------------------------------------------------------------------------------------------------- */
 
+static class JsInitHelperType
+{
+public:
+	JsInitHelperType() { JS_Init(); }
+	~JsInitHelperType() { JS_ShutDown(); }
+} JsInitHelper;
+
 struct _PolkitBackendJsAuthorityPrivate
 {
   gchar **rules_dirs;
@@ -458,7 +465,6 @@ polkit_backend_common_js_authority_finalize (GObject *object)
   delete authority->priv->js_polkit;
 
   JS_DestroyContext (authority->priv->cx);
-  /* JS_ShutDown (); */
 
   G_OBJECT_CLASS (polkit_backend_js_authority_parent_class)->finalize (object);
 }
@@ -490,7 +496,6 @@ polkit_backend_js_authority_class_init (PolkitBackendJsAuthorityClass *klass)
   polkit_backend_common_js_authority_class_init_common (klass);
 
   g_type_class_add_private (klass, sizeof (PolkitBackendJsAuthorityPrivate));
-  JS_Init ();
 }
 
 /* ---------------------------------------------------------------------------------------------------- */
