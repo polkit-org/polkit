89ec8974fda51fc3220875c3ecab239d1d416421
Luca Boccassi <bluca@debian.org>
2024-08-09 00:44:19 +0100

    Cast away 'volatile' when passing variable to g_once_init_enter()
    warning: argument 2 of ‘__atomic_load’ discards ‘volatile’ qualifier

--- src/polkit/polkitidentity.c
+++ src/polkit/polkitidentity.c
@@ -47,7 +47,7 @@ polkit_identity_get_type (void)
 {
   static volatile gsize g_define_type_id__volatile = 0;
 
-  if (g_once_init_enter (&g_define_type_id__volatile))
+  if (g_once_init_enter ((gsize*) &g_define_type_id__volatile))
     {
       static const GTypeInfo info =
       {
