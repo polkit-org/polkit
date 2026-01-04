c6e577e200559ef66b058aaed8a6a7785210dc85
Gleb Popov <6yearold@gmail.com>
2026-01-04 19:02:13 +0300

    Make it possible to run tests without unshare on Linux and on other OSes

--- test/wrapper.py
+++ test/wrapper.py
@@ -3,7 +3,9 @@
 import argparse
 import atexit
 import os
+import platform
 import subprocess
+import shutil
 import sys
 import signal
 import time
@@ -34,15 +36,16 @@ def setup_test_namespace(data_dir):
         subprocess.check_call(["mount", "--bind", os.path.join(data_dir, "etc"), "/etc"])
     except PermissionError:
         print("Lacking permissions to set up test harness, skipping")
-        sys.exit(77)
+        return False
     except AttributeError:
         print("Python 3.12 is required for os.unshare(), skipping")
-        sys.exit(77)
+        return False
     except OSError as e:
         if e.errno == errno.EINVAL:
             print("os.unshare(os.CLONE_NEWNS|os.CLONE_NEWUSER) not supported, skipping")
-            sys.exit(77)
+            return False
         raise
+    return True
 
 
 def stop_dbus(pid: int) -> None:
@@ -74,6 +77,23 @@ def stop_dbus(pid: int) -> None:
     signal.signal(signal.SIGTERM, signal.SIG_DFL)
 
 
+def setup_test_unconstrained_linux(data_dir):
+    shutil.copytree(os.path.join(data_dir, "etc"), "/etc", dirs_exist_ok=True)
+
+def setup_test_unconstrained_freebsd(data_dir):
+    shutil.copytree(os.path.join(data_dir, "etc"), "/etc", dirs_exist_ok=True)
+    with open("/etc/master.passwd.freebsd", "r") as passwd:
+        for entry in passwd:
+            # Retry if the passwd DB is locked by something else
+            while subprocess.call(["chpass", "-a", entry]) != 0:
+                pass
+    shutil.copy2("/etc/group.freebsd", "/etc/group")
+    # Ping ConsoleKit to get it autostarted, because some tests end up
+    # trying to access /var/lib/ConsoleKit/database
+    bus = dbus.SystemBus()
+    consolekit = bus.get_object("org.freedesktop.ConsoleKit", "/org/freedesktop/ConsoleKit/Manager")
+    consolekit.GetSeats(dbus_interface="org.freedesktop.ConsoleKit.Manager")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("test_executable",
@@ -84,7 +104,20 @@ if __name__ == "__main__":
                         help="set up a mock system D-Bus using dbusmock")
     args = parser.parse_args()
 
-    setup_test_namespace(args.data_dir)
+    if not setup_test_namespace(args.data_dir):
+        # We might fail to setup an isolated environment for running tests
+        # This might happen on Linux without unshare support or
+        # on non-Linux OSes, like FreeBSD.
+        # In this case, skip running tests by default, but allow them to be
+        # executed if the user asks for it via special env var
+        if os.getenv("ALLOW_SYSTEM_DESTRUCTIVE_TESTS") == "1":
+            if platform.system() == 'FreeBSD':
+                setup_test_unconstrained_freebsd(args.data_dir)
+            else:
+                setup_test_unconstrained_linux(args.data_dir)
+        else:
+            # skip tests
+            sys.exit(77)
 
     if args.mock_dbus:
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
